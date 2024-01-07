3b5baa6a6b506a338d6bfd6779bf9b68e50184bc
Suvro Bhowmick <107876156+Elitsuv@users.noreply.github.com>
2024-01-07 22:47:10 +0530

    Added Strings.py

---
+++ Introduction/Basic/1. Strings/Strings.py
@@ -0,0 +1,7 @@
+#Strings is a built in data types to print characters.
+#Strings are immutable as they once created cannot be changed, "Strings are stored under double or single quotation"
+
+#Example 1
+
+name = 'am a string'
+print(name + ' Ok Gotcha!')
