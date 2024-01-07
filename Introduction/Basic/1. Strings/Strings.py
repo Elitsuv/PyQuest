7ab269ace881a73ac58969d9cafa25c8008f7c6c
Suvro Bhowmick <107876156+Elitsuv@users.noreply.github.com>
2024-01-08 00:15:18 +0530

    Update Strings.py

--- Introduction/Basic/1. Strings/Strings.py
+++ Introduction/Basic/1. Strings/Strings.py
@@ -1,7 +1,9 @@
-#Strings is a built in data types to print characters.
-#Strings are immutable as they once created cannot be changed, "Strings are stored under double or single quotation"
-
-#Example 1
+#Example of Stings
 
 name = 'am a string'
 print(name + ' Ok Gotcha!')
+
+
+
+#Strings is a built in data types to print characters.
+#Strings are immutable as they once created cannot be changed, "Strings are stored under double or single quotation"
