22ccd3e9ff780b822e494b41e06097e3d6f8f749
Suvro Bhowmick <107876156+Elitsuv@users.noreply.github.com>
2024-01-07 22:59:58 +0530

    Create Errors-String.py

---
+++ Introduction/Basic/1. Strings/Errors-String.py
@@ -0,0 +1,17 @@
+#It is to be noted that anything stored under string becomes a 'Character' even numbers becomes string as shown below!
+
+#For Example
+
+num1 = 8
+num2 = 2
+
+print(num1 + num2);
+#Output: 10
+
+#Error in Numbers While Using String
+num1 = '8'
+num2 = '2'
+print(num1 + num2);
+
+#Output: 82
+#(adding up the two integers and not number!)
