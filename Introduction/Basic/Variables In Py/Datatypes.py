c133e5fb8800c63c8ff33d6e95d93513e3468a83
Suvro Bhowmick <107876156+Elitsuv@users.noreply.github.com>
2024-01-16 22:16:41 +0530

    Create Datatypes.py
    to-be-updated

---
+++ Introduction/Basic/Variables In Py/Datatypes.py
@@ -0,0 +1,14 @@
+#Data types are a fundamental concept that categorizes and defines the type of data that a variable can hold. 
+#Different programming languages support various data types, each with specific characteristics and operations
+
+#Integers
+#1. Numeric Types: int: Integer type represents whole numbers.
+x = 4
+x = float(5)
+x = int(9)
+
+#2. Character: text based datatypes
+name = "William"
+
+#3. Boolean Type: bool: Boolean type represents truth values, either True or False.
+num = false
