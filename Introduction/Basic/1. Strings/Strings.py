db61a392139e92fc5df390d662f5f44233f06681
Suvro Bhowmick <107876156+Elitsuv@users.noreply.github.com>
2024-01-30 21:52:06 +0530

    Delete Introduction/Basic/1. Strings directory

--- Introduction/Basic/1. Strings/Strings.py
+++
@@ -1,9 +0,0 @@
-#Example of Stings
-
-name = 'am a string'
-print(name + ' Ok Gotcha!')
-
-
-
-#Strings is a built in data types to print characters.
-#Strings are immutable as they once created cannot be changed, "Strings are stored under double or single quotation"
